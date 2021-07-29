b65fe7df1efe0cda124e83fee8314561b4516dbb
victornest <vnestbox@gmail.com>
2021-07-29 18:40:32 +0200

    PereborChallengeIndicator -  increased version

--- KG_PereborChallengeIndicator.user.js
+++ KG_PereborChallengeIndicator.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           KG_PereborChallengeIndicator
-// @version        0.2.1
+// @version        1.0.0
 // @namespace      klavogonki
 // @author         vnest
 // @description    Индикатор выполненной за сутки нормы 90/95% от рекорда (или поставленного рекорда) у игроков во время заезда
